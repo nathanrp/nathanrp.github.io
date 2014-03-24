c77af68915578a7814002c056d8de8f69aa7b32e
Nathan <nathanphilpot@gmail.com>
2014-03-24 16:45:59 -0600

    update

--- inc/-brains.php
+++ inc/-brains.php
@@ -5,9 +5,6 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=0, maximum-scale=1, minimum-scale=1.0, maximum-scale=1.0" media="(device-height: 568px)" />
-<meta name="apple-mobile-web-app-title" content="Nathan Philpot"/>
-<meta name="apple-mobile-web-app-capable" content="yes"/>
-<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
 
 <title>Nathan Philpot, Design is Honest</title>
 
