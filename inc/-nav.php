7cf703cb18bd4b130e2243f3d1919d1cbb356aab
Nathan <nathanphilpot@gmail.com>
2015-06-18 00:57:29 -0600

    updates

--- inc/-nav.php
+++ inc/-nav.php
@@ -1,5 +1,6 @@
 <nav class="nav">
 	<h1><a href="<?php echo $path;?>">Nathan Philpot</a></h1>
+	<h2>User Experience &amp; User Interface Design <a href="https://twitter.com/TCS_News"><abbr>@TCS</abbr></a></h2>
 	<a href="<?php echo $path;?>" class="<?php if($page==1) echo 'active';?>">Process</a>
 <!--	<a href="<?php echo $path;?>work" class="<?php if($page>=2 && $page<3) echo 'active';?>">Work</a> -->
 	<a href="<?php echo $path;?>contact" class="<?php if($page==3) echo 'active';?>">Contact</a>
