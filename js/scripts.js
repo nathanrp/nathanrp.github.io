8fa61bf10fe597a7b836301916eea334fe415219
Nathan <nathanphilpot@gmail.com>
2021-08-27 11:33:26 -0600

    scroll top

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,25 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle('dark');
 }
+
+$( document ).ready(function() {
+    console.log('ready');
+    var navH = $('nav ul:first-child').height();
+
+    $(window).scroll(function (event) {
+        console.log(navH);
+        var scroll = $(window).scrollTop();
+        if (scroll>=navH) {
+            $('.totop').addClass('show');
+        } else {
+            $('.totop').removeClass('show');
+        }
+    });
+
+    $('.totop').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+          }, 220);
+    });
+
+});
--- work/-nav.php
+++
@@ -1,5 +0,0 @@
-<nav class="nav work">
-	<a href="<?php echo $path;?>work" class="<?php if($page==2.1) echo 'active';?>"><abbr>WGU</abbr></a>
-	<a href="<?php echo $path;?>work/hrb.php" class="<?php if($page==2.2) echo 'active';?>"><abbr>H&amp;R</abbr> Block</a>
-	<a href="<?php echo $path;?>work/tcs.php" class="<?php if($page==2.3) echo 'active';?>"><abbr>TCS</abbr></a>
-</nav>
