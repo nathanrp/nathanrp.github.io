502bd5a65dc8e64a1f103aabadf1b40b3658ddad
Nathan <nathanrp@users.noreply.github.com>
2023-08-29 12:51:12 -0600

    Redesign

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,3 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle('dark');
 }
-
-$( document ).ready(function() {
-    console.log('ready');
-    var navH = $('.gallery figure:first-child').height();
-
-    $(window).scroll(function (event) {
-        console.log(navH);
-        var scroll = $(window).scrollTop();
-        if (scroll>=navH) {
-            $('.totop').addClass('show');
-        } else {
-            $('.totop').removeClass('show');
-        }
-    });
-
-    $('.totop').click(function(){
-        $('html, body').animate({
-            scrollTop: 0
-          }, 220);
-    });
-
-});
